40092aaadf3653be57b69ed67ea5fb52b16f799e
Mattias Geniar <m@ttias.be>
2019-03-20 22:13:32 +0100

    Topic detail view: fix basic template

--- resources/views/mailinglist/showTopic.blade.php
+++ resources/views/mailinglist/showTopic.blade.php
@@ -6,33 +6,54 @@
 
         <title>{{ $mailingList->slug }}: {{ $topic->topic }}</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
         <!-- Styles -->
-        <link rel="stylesheet" href="/css/custom.css" />
+        <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
     </head>
+
     <body>
-        <div class="flex-center position-ref">
-            <div>
-                <div class="title m-b-md">
-                    {{ $mailingList->name }}: {{ $topic->topic }}
-                </div>
+        <div class="font-sans">
+            <!-- title -->
+            <div class="bg-white max-w-xl mx-auto my-8">
+                <h2 class="font-medium text-2lg ml-24 mb-2 mx-auto">{{ $mailingList->name }}: {{ $topic->topic }}</h2>
+            </div>
 
-                <div class="links">
-                    @foreach ($topic->messages as $message)
-                        <img src="{{ $message->author->getGravatarAttribute() }}" />
-                        From: {{ $message->author->display_name }}<br />
-                        Date: {{ $message->created_at }}<br />
+            @foreach ($topic->messages as $message)
+            <!-- mail message -->
+            <div class="bg-white max-w-xl mx-auto my-8 border border-grey-light">
+                <div class="flex pt-4 px-4">
+                    <div class="w-16 mr-2">
+                        <img class="p-2 rounded rounded-full" src="{{ $message->author->getGravatarAttribute() }}">
+                    </div>
 
-                        <pre>
-{{ $message->content }}
-                        </pre>
+                    <div class="px-2 pt-2 flex-grow">
+                        <header>
+                            <a href="/mailing-list/author/{{ $message->author->id }}" class="text-black no-underline">
+                                <span class="font-medium">{{ $message->author->display_name }}</span>
+                            </a>
 
-                        <hr class="post-seperator"/>
-                    @endforeach
+                            <div class="text-xs text-grey flex items-center my-1">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                class="h-4 w-4 mr-1 feather feather-calendar">
+                                <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
+                                <line x1="16" y1="2" x2="16" y2="6"></line>
+                                <line x1="8" y1="2" x2="8" y2="6"></line>
+                                <line x1="3" y1="10" x2="21" y2="10"></line>
+                                </svg>
+                                <span title="{{ $message->created_at }}">{{ $message->created_at->ago() }}</span>
+                            </div>
+
+                        </header>
+
+                        <article class="py-4 text-grey-darkest">
+                            {!! getMessageBody(e($message->content)) !!}
+                        </article>
+
+                    </div>
                 </div>
             </div>
+            @endforeach
         </div>
     </body>
 </html>
+
